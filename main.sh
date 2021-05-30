37a29e2804f9811d6c77d4a61ce39900f4bfd5d8
JH <>
2021-05-30 20:38:06 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,42 @@
+echo Hello World
+
+#config git global
+git config --global user.mail "mail"
+git config --global user.name "name"
+
+git push 
+
+#delet repository
+rm -rf.git
+
+#sql
+/Applications/MAMP/Library/bin/mysql -u root -p
+#Password: root
+
+#Python
+/usr/local/bin/python3
+
+#push something to GitHub
+echo "# GITHUB" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Username/Repositoryname.git
+git push -u origin main
+
+#some Terminal commands
+Arp
+Cal
+Vim
+Pwd
+Cd
+Cd ..
+Ifconfig
+Ls
+Netstat
+Traceroute (Name der Webseite)
+TMPDIR 
+Nslookup
+
+
